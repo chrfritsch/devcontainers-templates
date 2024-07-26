c33de41935cc320a61184c34b7041e48bb3259f8
Christian Fritsch <christian.fritsch@burda.com>
2024-07-26 12:04:51 +0000

    chore: Remove commented out test in test.sh

--- test/drupal-project/test.sh
+++ test/drupal-project/test.sh
@@ -3,7 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "distro" lsb_release -c
+#check "distro" lsb_release -c
 #check "greeting" [ $(cat /usr/local/etc/greeting.txt | grep hey) ]
 
 # Report result
