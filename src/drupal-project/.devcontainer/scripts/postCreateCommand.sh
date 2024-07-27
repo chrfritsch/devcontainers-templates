30851382d22ae617300e7f38a7684264df556ec4
Christian Fritsch <christian.fritsch@burda.com>
2024-07-27 13:05:54 +0000

    chore: Make drupal testing work

--- src/drupal-project/.devcontainer/scripts/postCreateCommand.sh
+++ src/drupal-project/.devcontainer/scripts/postCreateCommand.sh
@@ -5,9 +5,6 @@ composer install
 
 # Setup docroot
 DOCROOT=$(composer config extra.drupal-scaffold.locations.web-root)
-echo "Setting up docroot: ${DOCROOT}"
-rm -rf /var/www/html
-ln -s "$(pwd)/${DOCROOT}" /var/www/html
-
-# Start apache
-apache2ctl start
+sudo chmod a+x "$(pwd)"
+sudo rm -rf /var/www/html
+sudo ln -s "$(pwd)/${DOCROOT}" /var/www/html
